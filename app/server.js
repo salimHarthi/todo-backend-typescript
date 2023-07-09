66e87fd1d2950fa52ae8aa48bf8957526b130998
salimHarthi <salim.h.harthi@gmail.com>
2023-07-09 19:00:43 +0400

    removed db this is for a react training and i want somthing easy to estup

--- app/server.js
+++ app/server.js
@@ -1,64 +1,70 @@
 /// <reference path="../typings/express/express.d.ts"/>
 /// <reference path="../typings/body-parser/body-parser.d.ts"/>
-var app = require('express')(), bodyParser = require('body-parser'), backend = require('./backend');
+const app = require('express')();
+const bodyParser = require('body-parser');
+
 app.use(bodyParser.json());
 app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    res.header('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE');
-    res.header('Access-Control-Allow-Origin', '*');
-    next();
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE');
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
 });
-var todos = backend(process.env.DATABASE_URL);
+const todos = [{ id: -1, title: 'test', order: 'test1', completed: false }];
+let id = 0;
 function createCallback(res, onSuccess) {
-    return function callback(err, data) {
-        if (err || !data) {
-            res.send(500, 'Something bad happened!');
-            return;
-        }
-        onSuccess(data);
-    };
+  return function callback(err, data) {
+    if (err || !data) {
+      res.send(500, 'Something bad happened!');
+      return;
+    }
+    onSuccess(data);
+  };
 }
-function createTodo(req, todo) {
-    return {
-        title: todo.title,
-        order: todo.order,
-        completed: todo.completed || false,
-        url: req.protocol + '://' + req.get('host') + '/' + todo.id
-    };
-}
-function getCreateTodo(req) {
-    return function (todo) {
-        return createTodo(req, todo);
-    };
-}
+
 app.get('/', function (req, res) {
-    todos.all(createCallback(res, function (todos) {
-        res.send(todos.map(getCreateTodo(req)));
-    }));
+  res.json(todos);
 });
 app.get('/:id', function (req, res) {
-    todos.get(req.params.id, createCallback(res, function (todo) {
-        res.send(createTodo(req, todo));
-    }));
+  var index = todos.findIndex((x) => x.id == Number(req.params.id));
+  if (index > -1) {
+    res.json(todos[index]);
+  } else {
+    res.json(null);
+  }
 });
 app.post('/', function (req, res) {
-    todos.create(req.body.title, req.body.order, createCallback(res, function (todo) {
-        res.send(createTodo(req, todo));
-    }));
+  let data = {
+    id: id++,
+    title: req.body.title,
+    order: req.body.order,
+    completed: req.body.completed || false,
+  };
+  todos.push(data);
+  res.json(data);
 });
 app.patch('/:id', function (req, res) {
-    todos.update(req.params.id, req.body, createCallback(res, function (todo) {
-        res.send(createTodo(req, todo));
-    }));
+  var foundIndex = todos.findIndex((x) => x.id == Number(req.params.id));
+  if (foundIndex > -1) {
+    todos[foundIndex] = {
+      id: Number(req.params.id),
+      title: req.body.title,
+      order: req.body.order,
+      completed: req.body.completed,
+    };
+  }
+
+  res.json(todos[foundIndex]);
 });
 app.delete('/', function (req, res) {
-    todos.clear(createCallback(res, function (todos) {
-        res.send(todos.map(getCreateTodo(req)));
-    }));
+  todos.length = 0;
+  res.json(todos);
 });
 app.delete('/:id', function (req, res) {
-    todos.delete(req.params.id, createCallback(res, function (todo) {
-        res.send(createTodo(req, todo));
-    }));
+  let index = todos.findIndex((x) => x.id == Number(req.params.id));
+  if (index > -1) {
+    todos.splice(index, 1);
+  }
+  res.json(todos);
 });
 app.listen(Number(process.env.PORT || 5000));
